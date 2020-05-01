80a7b0f8690a59717a1325f6bf302d29dabaff0c
theLance <lance.alter@gmail.com>
2020-05-01 21:12:59 +0200

    Death timer and elapsed time measurer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,42 @@
 #include "thegame.hpp"
 
+#include <chrono>
+#include <thread>
+
+double TOTAL_TIME = 5;
+
+class Timer
+{
+public:
+    Timer() : startTime(std::chrono::steady_clock::now()) {}
+    auto elapsed() {
+        auto now = std::chrono::steady_clock::now();
+        return std::chrono::duration_cast<std::chrono::seconds>(now - startTime).count();
+    }
+
+    bool isOver() {
+        std::cout << "elapsed=" << elapsed() << std::endl;
+        return elapsed() >= TOTAL_TIME;
+    }
+private:
+    std::chrono::time_point<std::chrono::steady_clock> startTime;
+};
+
+class DeathTimer
+{
+public:
+    struct GameOver{};
+    DeathTimer() {
+        std::thread timer([]() {
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            throw GameOver();
+        });
+        timer.detach();
+    }
+private:
+};
+
+
 int main()
 {
     std::cout << "Choose 'm' for movie titles, "
@@ -19,17 +56,18 @@ int main()
 
     std::string select;
     int cnt = 0;
+    //DeathTimer dt;
     while(select != "q") {
-        getline(std::cin, select);
         auto word(wordList.pop());
         while(cache.isIn(word)) {
             word = wordList.pop();
         }
-        if(select == ".")
+        if(select == ".") {
             cnt++;
+        }
         cache.add(word);
         std::cout << "    [" << word << "]     " << cnt << std::endl;
+        getline(std::cin, select);
     }
-
     return 0;
 }
